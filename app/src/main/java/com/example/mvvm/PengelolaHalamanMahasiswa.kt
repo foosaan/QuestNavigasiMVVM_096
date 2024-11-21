3dc10d6370ffddae57e8bb61cd7d4f2e9eb3cb26
fauzan alfikri <112147642+foosaan@users.noreply.github.com>
2024-11-21 22:59:19 +0700

    menambahkan modifier untuk menyesuaikan tampilan

--- app/src/main/java/com/example/mvvm/PengelolaHalamanMahasiswa.kt
+++ app/src/main/java/com/example/mvvm/PengelolaHalamanMahasiswa.kt
@@ -1,6 +1,15 @@
 package com.example.mvvm
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
 enum class Halaman{
     Formulir,
     Detail,
+}
+
+@Composable
+fun PengelolaHalamanMahasiswa(
+    modifier: Modifier = Modifier,
+){
 }
