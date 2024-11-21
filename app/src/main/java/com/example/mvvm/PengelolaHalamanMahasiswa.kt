b09196ff52a87f78c97d642efe40635cb672ce24
fauzan alfikri <112147642+foosaan@users.noreply.github.com>
2024-11-21 23:02:31 +0700

    menambahkan scaffold sebagai rangka kerja dasar

--- app/src/main/java/com/example/mvvm/PengelolaHalamanMahasiswa.kt
+++ app/src/main/java/com/example/mvvm/PengelolaHalamanMahasiswa.kt
@@ -1,7 +1,15 @@
 package com.example.mvvm
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.example.mvvm.ui.vievmodel.MahasiswaViewModel
 
 enum class Halaman{
     Formulir,
@@ -11,5 +19,15 @@ enum class Halaman{
 @Composable
 fun PengelolaHalamanMahasiswa(
     modifier: Modifier = Modifier,
+    navHost: NavHostController = rememberNavController(),
+    viewModel: MahasiswaViewModel = viewModel()
 ){
+    Scaffold { isipadding ->
+        val uiState by viewModel.uiState.collectAsState()
+        NavHost(
+            modifier = modifier.padding(isipadding),
+            navController = navHost, startDestination = Halaman.Formulir.name
+        ) {
+        }
+    }
 }
